fd4eb830cfaf5e068e9b125952a3cae05ed66ebe
ftnext <takuyafjp+develop@gmail.com>
2024-11-15 16:21:18 +0900

    [test] 残りのテストを追加

--- goal/tests/test_core.py
+++ goal/tests/test_core.py
@@ -3,3 +3,15 @@ from fizzbuzz.core import fizzbuzz
 
 def test_3の倍数のときはFizzを返す():
     assert fizzbuzz(3) == "Fizz"
+
+
+def test_5の倍数のときはBuzzを返す():
+    assert fizzbuzz(5) == "Buzz"
+
+
+def test_15の倍数のときはFizzBuzzを返す():
+    assert fizzbuzz(15) == "FizzBuzz"
+
+
+def test_3の倍数でも5の倍数でもないときは数字の文字列を返す():
+    assert fizzbuzz(1) == "1"
